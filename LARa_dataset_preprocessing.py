5c7e452f0ad21d45b54cfe976583a622b34e382b
Nilah Ravi Nair <nilahnair.27@gmail.com>
2024-01-24 09:10:50 +0100

    Update LARa_dataset_preprocessing.py

--- LARa_dataset_preprocessing.py
+++ LARa_dataset_preprocessing.py
@@ -633,7 +633,7 @@ def generate_data(ids, sliding_window_length, sliding_window_step, data_dir=None
         recordings = ['R11', 'R12', 'R13', 'R16', 'R18', 'R19']
     elif usage_modus == 'test':
         #recordings = ['R14', 'R15', 'R17']
-        recordings = ['R01', 'R02', 'R03', 'R04', 'R05', 'R06', 'R07', 'R08', 'R09', 'R10', 'R11', 'R12', 'R13', 'R14', 'R15', 'R16', 'R17', 'R18', 'R19' 'R20', 'R21', 'R22', 'R23', 
+        recordings = ['R01', 'R02', 'R03', 'R04', 'R05', 'R06', 'R07', 'R08', 'R09', 'R10', 'R11', 'R12', 'R13', 'R14', 'R15', 'R16', 'R17', 'R18', 'R19', 'R20', 'R21', 'R22', 'R23', 
                       'R24', 'R25', 'R26', 'R27', 'R28', 'R29', 'R30']
     
 
