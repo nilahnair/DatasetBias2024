4bd0352d58d120df3bf353f24bff356dab27643f
Nilah Ravi Nair <nilahnair.27@gmail.com>
2024-01-25 13:59:17 +0100

    Update LARa_dataset_preprocessing.py

--- LARa_dataset_preprocessing.py
+++ LARa_dataset_preprocessing.py
@@ -873,9 +873,9 @@ def create_dataset(half=True):
     '''
     
     #all_data = ["S01", "S02", "S03", "S04", "S05", "S06", "S07", "S08", "S09", "S10", "S11", "S12", "S13", "S14", "S15", "S16" ]
-    train_ids = ["S02", "S06", "S13", "S16"] #2,6,13,16
-    val_ids = ["S02", "S06", "S13", "S16"]
-    test_ids = ["S01", "S03", "S04", "S05", "S07", "S08", "S09", "S10", "S11", "S12", "S14", "S15"]
+    train_ids = ["S04", "S06", "S08", "S11"] #4,6,8,11
+    val_ids = ["S04", "S06", "S08", "S11"]
+    test_ids = ["S01", "S02", "S03", "S05", "S07", "S09", "S10", "S12", "S13", "S14", "S15", "S16"]
     
     
     #general_statistics(train_ids)
@@ -888,7 +888,7 @@ def create_dataset(half=True):
     else:
         "Path to the segmented sequences"
         print('no downsampling')
-        base_directory = '/data/nnair/datasetbias/lara/prepros/exp3/'
+        base_directory = '/data/nnair/datasetbias/lara/prepros/exp5/'
         sliding_window_length = 200
         sliding_window_step = 25
 
