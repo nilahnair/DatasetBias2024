85243e99b3397283477c59f68c90797fba20081b
Nilah Ravi Nair <nilahnair.27@gmail.com>
2024-02-02 07:56:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def load_credentials(path='~/.mongodb_credentials'):
 
 user, pw = load_credentials(path='~/.mongodb_credentials')
 
-ex= Experiment('lara cnn x3 exp19')
+ex= Experiment('lara lstm x3 exp19')
 
 
 ex.observers.append(MongoObserver.create(url='curtiz',
@@ -221,7 +221,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
     
     if output[output_idx] == 'softmax':
         labeltype = "class"
-        folder_exp = {'mocap': "/data/nnair/datasetbias/lara/results/exp19/cnn/",
+        folder_exp = {'mocap': "/data/nnair/datasetbias/lara/results/exp19/lstm/",
                     'mbientlab': "/data/nnair/icpr2024/lara_imu/results/transt/",
                     'mobiact': "/data/nnair/icpr2024/mobiact/results/trial1/",
                     'motionsense': "/data/nnair/datasetbias/motionsense/results/exp5/cnntrans/",
@@ -243,7 +243,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                     }
 
     # GPU
-    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
     GPU = 0
 
     # Labels position on the segmented window
@@ -357,7 +357,7 @@ def setup_experiment_logger(logging_level=logging.DEBUG, filename=None):
 def my_config():
     print("configuration function began")
     config = configuration(dataset_idx=0,
-                           network_idx=2,
+                           network_idx=1,
                            output_idx=0,
                            usage_modus_idx=0,
                            #dataset_fine_tuning_idx=0,
