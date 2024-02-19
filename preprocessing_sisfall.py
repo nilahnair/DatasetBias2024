1258ca8dae90123b86685faf882530f08923d5fa
Nilah Ravi Nair <nilahnair.27@gmail.com>
2024-02-19 11:16:17 +0100

    Update preprocessing_sisfall.py

--- preprocessing_sisfall.py
+++ preprocessing_sisfall.py
@@ -462,7 +462,7 @@ def main():
                'SE03':26, 'SE04':27, 'SE05':28, 'SE06':29, 'SE07':30, 'SE08':31, 'SE09':32, 'SE10':33, 'SE11':34, 'SE12':35, 'SE13':36, 'SE14':37, 'SE15':38}
     
     train_ids=[]
-    for i in [5, 17, 25, 26, 27, 31, 33, 38]:
+    for i in [12, 15, 16, 22, 29, 30, 32, 35]:
         x = [sub_list for sub_list, v in sub_list.items() if v == i][0]
         train_ids.append(x)
         
@@ -477,7 +477,7 @@ def main():
     activities= ['D01', 'D02', 'D03', 'D04', 'D05', 'D07', 'D08', 'D09', 
                  'D10', 'D011', 'D12', 'D14', 'D15', 'D16', 'D17']
     
-    base_directory ='/data/nnair/datasetbias/sisfall/prepros/exp18/'
+    base_directory ='/data/nnair/datasetbias/sisfall/prepros/exp17/'
     data_dir_train = base_directory + 'sequences_train/'
     data_dir_val = base_directory + 'sequences_val/'
     data_dir_test = base_directory + 'sequences_test/'
