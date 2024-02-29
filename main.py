4b441f93c8882f5e4530ea777423b32509feba2b
Nilah Ravi Nair <nilahnair.27@gmail.com>
2024-02-29 17:33:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def load_credentials(path='~/.mongodb_credentials'):
 
 user, pw, url, db_name = load_credentials(path='~/.mongodb_credentials')
 
-ex= Experiment('mobiact cnntrans x3 exp5')
+ex= Experiment('mobiact cnn x3 exp6')
 
 
 ex.observers.append(MongoObserver.create(url=url,
@@ -134,7 +134,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                             'lstm': {'softmax': 15, 'attribute': 10},
                             'cnn_imu': {'softmax': 30, 'attribute': 10},
                             'cnn_transformer':{'softmax': 50, 'attribute': 6}},
-              'mobiact': {'cnn': {'softmax': 30, 'attribute': 50},
+              'mobiact': {'cnn': {'softmax': 15, 'attribute': 50},
                           'lstm': {'softmax': 15, 'attribute': 5},
                           'cnn_imu': {'softmax': 32, 'attribute': 50},
                           'cnn_transformer':{'softmax': 15, 'attribute': 6}},
@@ -223,7 +223,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
         labeltype = "class"
         folder_exp = {'mocap': "/data/nnair/datasetbias/lara/results/exp32/cnntrans/",
                     'mbientlab': "/data/nnair/icpr2024/lara_imu/results/transt/",
-                    'mobiact': "/data/nnair/datasetbias/mobiact/results/exp5/cnntrans/",
+                    'mobiact': "/data/nnair/datasetbias/mobiact/results/exp6/cnn/",
                     'motionsense': "/data/nnair/datasetbias/motionsense/results/exp5/cnntrans/",
                     'sisfall': "/data/nnair/datasetbias/sisfall/results/exp33/cnntrans/"
                     }
@@ -237,13 +237,13 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
     
     dataset_root = {'mocap': "/data/nnair/datasetbias/lara/prepros/exp32/",
                     'mbientlab': "/data/nnair/icpr2024/lara_imu/prepros/",
-                    'mobiact': "/data/nnair/datasetbias/mobiact/prepros/exp5/",
+                    'mobiact': "/data/nnair/datasetbias/mobiact/prepros/exp6/",
                     'motionsense': "/data/nnair/datasetbias/motionsense/prepros/exp5/",
                     'sisfall': "/data/nnair/datasetbias/sisfall/prepros/exp33/"
                     }
 
     # GPU
-    os.environ["CUDA_VISIBLE_DEVICES"] = "7"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "4"
     GPU = 0
 
     # Labels position on the segmented window
@@ -357,7 +357,7 @@ def setup_experiment_logger(logging_level=logging.DEBUG, filename=None):
 def my_config():
     print("configuration function began")
     config = configuration(dataset_idx=2,
-                           network_idx=3,
+                           network_idx=0,
                            output_idx=0,
                            usage_modus_idx=0,
                            #dataset_fine_tuning_idx=0,
