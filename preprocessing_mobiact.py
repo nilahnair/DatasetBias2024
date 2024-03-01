fde603157d438ab2e82287680fcec301478da69f
Nilah Ravi Nair <nilahnair.27@gmail.com>
2024-03-01 13:47:28 +0100

    Update preprocessing_mobiact.py

--- preprocessing_mobiact.py
+++ preprocessing_mobiact.py
@@ -470,7 +470,7 @@ def create_dataset(identity_bool = False):
     
     
     train_ids=[]
-    for i in [ 5, 11, 16, 19, 23, 24, 26, 40, 47, 54, 60, 62]:
+    for i in [ 3, 5, 10, 11, 19, 22, 26, 35, 40, 49, 61, 62]:
         x = [sub_list for sub_list, v in sub_list.items() if v == i][0]
         train_ids.append(x)
         
@@ -480,7 +480,7 @@ def create_dataset(identity_bool = False):
             test_ids.append(i)
     
     
-    base_directory = '/data/nnair/datasetbias/mobiact/prepros/exp19/'
+    base_directory = '/data/nnair/datasetbias/mobiact/prepros/exp20/'
     print('train ids')
     print(train_ids)
     print('test ids')
